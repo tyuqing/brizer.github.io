327631312a389fbd6d0f4f06867805f801558b35
brizer <362512489@qq.com>
2016-11-29 11:39:04 +0800

    Site updated: 2016-11-29 11:38:49

---
+++ demo/expressDemo/myapp/config/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by liufang on 2016/11/29.
+ */
+module.exports = {
+    port:3000,
+    dbUrl:'mongodb://localhost/myapp'
+}
---
+++ demo/expressDemo/myapp/start.js
@@ -0,0 +1,29 @@
+/**
+ * Created by liufang on 2016/11/29.
+ */
+var debug = require('debug')('myapp');
+var mongoose = require('mongoose');
+
+var app = require('./app');
+var config = require('./config/config');
+
+app.set('config',config);
+
+//启动mongo
+mongoose.connect(app.get('config').dbUrl);
+mongoose.connection.on('connected',function(){
+    console.log('connect to the mongodb success');
+});
+mongoose.connection.on('error',function(err){
+    console.log('connnect to the mongodb error:'+err);
+    process.exit(1);
+});
+mongoose.connection.on('disconnected',function(){
+    console.log('disconnect mongodb');
+    process.exit(1);
+});
+
+app.listen(app.get('config').port || 3000,function(){
+    console.log('Express server start success');
+    debug('Express server listening on port '+ app.get('config').port || 3000);
+})
